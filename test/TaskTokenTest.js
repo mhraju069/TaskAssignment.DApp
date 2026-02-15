e806883e1519840d5d4f538e5314e1059d5d367c
mhraju069 <mhraju069@gmail.com>
2026-02-15 17:04:18 +0600

    Add new test Should have the correct initial supply

--- test/TaskTokenTest.js
+++ test/TaskTokenTest.js
@@ -6,15 +6,22 @@ describe("TaskToken", function () {
     let owner
     let instance
 
+    
     before(async function () {
         [owner, user] = await ethers.getSigners();
         const Token = await ethers.getContractFactory("TaskToken");
         instance = await Token.deploy(1000);
     });
 
+
     it("Should have the correct name and symbol", async function () {
         expect(await instance.name()).to.equal("TaskToken");
         expect(await instance.symbol()).to.equal("TT");
+    });
+
+
+    it("Should have the correct initial supply", async function () {
+        expect(await instance.totalSupply()).to.equal(ethers.parseEther("1000"));
     });
 
 });
