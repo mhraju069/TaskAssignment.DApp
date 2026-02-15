f3517772778bc7bfc989d1f39e152f001f9430ae
mhraju069 <mhraju069@gmail.com>
2026-02-15 14:29:30 +0600

    Add Ownable MintToken function and event for token minting to others

--- contracts/TaskToken.sol
+++ contracts/TaskToken.sol
@@ -3,10 +3,15 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract TaskToken is ERC20 , Ownable , ERC20Burnable {
+contract TaskToken is ERC20, Ownable, ERC20Burnable {
+    event Mint(address indexed to, uint amount);
 
-    constructor (uint totalSupply) ERC20("TaskToken","TT") Ownable(msg.sender){
-          _mint(msg.sender, totalSupply * 10 ** decimals());
+    constructor(uint totalSupply) ERC20("TaskToken", "TT") Ownable(msg.sender) {
+        _mint(msg.sender, totalSupply * 10 ** decimals());
     }
 
+    function MintToken(uint amount, address receiver) public onlyOwner {
+        _mint(receiver, amount);
+        emit Mint(receiver, amount);
+    }
 }
