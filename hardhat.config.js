96f14c02d2f9ff948348fce4bb0044f09970e4b4
mhraju069 <mhraju069@gmail.com>
2026-02-15 18:45:54 +0600

    Setup settings and and networks

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,27 @@
+require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
 require("solidity-coverage");
 
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      },
+      viaIR: true
+    }
+  },
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apikey: process.env.ETHERSCAN_API_KEY
+  }
 };
