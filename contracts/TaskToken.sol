a3d673f2d7a5893947abe843621e0cdd03f5d377
mhraju069 <mhraju069@gmail.com>
2026-02-15 14:38:03 +0600

    Add BurnToken function and event for burning tokens

--- contracts/TaskToken.sol
+++ contracts/TaskToken.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 contract TaskToken is ERC20, Ownable, ERC20Burnable {
     event Mint(address indexed to, uint amount);
+    event Burn(address indexed from, uint amount);
 
     constructor(uint totalSupply) ERC20("TaskToken", "TT") Ownable(msg.sender) {
         _mint(msg.sender, totalSupply * 10 ** decimals());
@@ -13,5 +14,10 @@ contract TaskToken is ERC20, Ownable, ERC20Burnable {
     function MintToken(uint amount, address receiver) public onlyOwner {
         _mint(receiver, amount);
         emit Mint(receiver, amount);
+    }
+
+    function BurnToken(uint amount) public {
+        _burn(msg.sender,amount);
+        emit Burn(msg.sender,amount);
     }
 }
